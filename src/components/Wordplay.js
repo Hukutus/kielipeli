118dcc34eefee7665c4a849045da15cb0a3dbbf4
Topi Salonen <topi@nordicedu.com>
2018-05-15 19:58:15 +0300

    Modify wordplay to show match and mismatch

--- src/components/Wordplay.js
+++ src/components/Wordplay.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import firestore from "../javascripts/firebase";
 import Timer from "./Timer";
 
@@ -9,96 +9,135 @@ class Wordplay extends Component {
     super();
 
     this.state = {
-        showTimer: true,
-        currentIndex: 0,
-        currentWord: {fi: "Testi", en: "Test", id: null},
-        wordpairs: []
+      showTimer: true,
+      currentIndex: 0,
+      currentWord: {fi: "Testi", en: "Test", id: null},
+      wordpairs: [],
+      selectionClass: ""
     };
 
     this.onTimerEnd = this.onTimerEnd.bind(this);
     this.shuffleArray = this.shuffleArray.bind(this);
+    this.confirmMatch = this.confirmMatch.bind(this);
   }
 
   componentDidMount() {
     firestore.collection(docRef).onSnapshot(snapshot => {
       const _wordpairs = snapshot.docs.map(doc => {
-        return { ...doc.data(), id: doc.id };
+        return {...doc.data(), id: doc.id};
       });
 
       const shuffledWords = this.shuffleArray(_wordpairs);
 
-      this.setState({ wordpairs: shuffledWords, currentWord: this.shuffleArray(shuffledWords)[0] });
+      this.setState({wordpairs: shuffledWords, currentWord: this.shuffleArray(shuffledWords)[0]});
     });
   }
 
   onTimerEnd() {
-    this.setState({ "showTimer": false });
+    this.setState({"showTimer": false});
   }
 
-  checkWordMatch(wordId) {
-    if (wordId === this.state.currentWord.id) {
-      this.setState({
-          wordpairs: this.shuffleArray(this.state.wordpairs),
-          currentWord: this.state.wordpairs[0]
-      });
-    }
-    else {
-      console.log("Wrong!");
-    }
+  confirmMatch() {
+    this.setState({
+      wordpairs: this.shuffleArray(this.state.wordpairs),
+      currentWord: this.shuffleArray(this.state.wordpairs)[0]
+    });
   }
 
   shuffleArray(array) {
-      let currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    const arrayClone = array.slice(0);
 
-      // While there remain elements to shuffle...
-      while (0 !== currentIndex) {
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
 
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex -= 1;
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-          // And swap it with the current element.
-          temporaryValue = array[currentIndex];
-          array[currentIndex] = array[randomIndex];
-          array[randomIndex] = temporaryValue;
-      }
+      // And swap it with the current element.
+      temporaryValue = arrayClone[currentIndex];
+      arrayClone[currentIndex] = arrayClone[randomIndex];
+      arrayClone[randomIndex] = temporaryValue;
+    }
 
-      return array;
+    return arrayClone;
   }
 
   render() {
     return (
-        <div className="Landing-body">
-            <div className="Landing-border">
-                <div className="Landing-header">
-                    <h1>Wordplay</h1>
-                </div>
+      <div className="Landing-body">
+        <div className="Landing-border">
+          <div className="Landing-header">
+            <h1>Wordplay</h1>
+          </div>
 
 
-                { !this.state.showTimer ?
-                    <div className="TimerContainer">
-                        <Timer onTimerEnd={this.onTimerEnd} timerSeconds={3}></Timer>
-                    </div> :
-                    <div>
-                      <div>
-                          <div className={"instructions"}>Select the correct pair for the given word!</div>
-                          <div className={"titleWord"}>{this.state.currentWord.en}</div>
-                      </div>
+          {!this.state.showTimer ?
+            <div className="TimerContainer">
+              <Timer onTimerEnd={this.onTimerEnd} timerSeconds={3}></Timer>
+            </div> :
+            <div>
+              <div>
+                <div className={"instructions"}>Select the correct pair for the given word!</div>
+                <div className={"titleWord"}>{this.state.currentWord.en}</div>
+              </div>
 
-                      <div className={"wordArea"}>
-                          {this.state.wordpairs.map(pair => {
-                              return <div className={"matchWord"} key={pair.id}
-                                          onClick={() => this.checkWordMatch(pair.id)}
-                              >{pair.fi}</div>;
-                          })}
-                      </div>
+              <div className={"wordArea"}>
+                {this.state.wordpairs.map(wordPair => {
+                  return (
+                    <Words currentWord={this.state.currentWord} pair={wordPair} key={wordPair.id} confirmMatch={this.confirmMatch}/>
+                  );
+                })}
+              </div>
 
-                    </div>
-                }
             </div>
+          }
         </div>
+      </div>
     );
   }
 }
 
 export default Wordplay;
+
+class Words extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      statusClass: ""
+    };
+
+    this.checkWordMatch = this.checkWordMatch.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.currentWord.id !== this.props.currentWord.id) {
+      this.setState({statusClass: ""});
+    }
+  }
+
+  checkWordMatch(wordId) {
+    if (wordId === this.props.currentWord.id) {
+      this.setState({
+        statusClass: "wordMatch"
+      });
+
+      this.props.confirmMatch();
+    }
+    else {
+      this.setState({
+        statusClass: "wordMismatch"
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className={"matchWord " + this.state.statusClass} onClick={() => this.checkWordMatch(this.props.pair.id)}>
+        {this.props.pair.fi}
+      </div>
+    );
+  }
+};
